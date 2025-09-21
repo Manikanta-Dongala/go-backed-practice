4c53fd0c296893cb1c9dab54aa70f391506fdf71
Manikanta Dongala <dongala565@gmail.com>
2025-09-21 13:41:26 +0530

    sending json object as response

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 func main() {
 	app := fiber.New()
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello, World!")
+	// })
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.Status(200).JSON(fiber.Map{"msg":"Hello world!"})
 	})
 	app.Listen(":3000")
 }
